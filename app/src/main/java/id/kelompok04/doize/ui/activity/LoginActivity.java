5e0fe4356c6466c684ccb1743b653c9e64fd8061
Samodra <38998978+saamodra@users.noreply.github.com>
2021-07-17 18:07:35 +0700

    Add login page

---
+++ app/src/main/java/id/kelompok04/doize/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package id.kelompok04.doize.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.kelompok04.doize.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
