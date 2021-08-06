c0a100a5ccbcb2aee20e51de32df4f3ac9c0346b
Samodra <38998978+saamodra@users.noreply.github.com>
2021-08-06 18:59:18 +0700

    fix : bug pomodoro alarm

--- app/src/main/java/id/kelompok04/doize/ui/fragment/PomodoroFragment.java
+++ app/src/main/java/id/kelompok04/doize/ui/fragment/PomodoroFragment.java
@@ -436,6 +436,12 @@ public class PomodoroFragment extends Fragment {
         resetAlert.show();
     }
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        stopAlarm();
+    }
+
     ItemTouchHelper.SimpleCallback mSimpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
