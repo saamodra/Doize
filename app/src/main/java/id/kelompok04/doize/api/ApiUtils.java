436342f631eb26a6dcbcfe4d4934b9c4f4b6e244
Rizki Kurniawati <rzkurniawati.s@gmail.com>
2021-08-11 20:09:56 +0700

    fix : update password

--- app/src/main/java/id/kelompok04/doize/api/ApiUtils.java
+++ app/src/main/java/id/kelompok04/doize/api/ApiUtils.java
@@ -9,8 +9,8 @@ import id.kelompok04.doize.service.DailyActivityService;
 import id.kelompok04.doize.service.UserService;
 
 public class ApiUtils {
-    public static final String API_URL = "http://192.168.43.145:8000/api/";
-//    public static final String API_URL = "http://10.0.2.2:8000/api/";
+//    public static final String API_URL = "http://192.168.43.145:8000/api/";
+    public static final String API_URL = "http://10.0.2.2:8000/api/";
 //    public static final String API_URL = "http://192.168.43.100:8000/api/";
 //    public static final String API_URL = "https://doize.herokuapp.com/api/";
 
